d4fafec856ee257e65e9d9f7b3e8e330fe035b9d
rshashank11 <shashankyadav2000@gmail.com>
2022-03-01 17:55:19 +0530

    fix: Fixed the bugs, style: Used chalk library

--- index.js
+++ index.js
@@ -2,23 +2,26 @@ var chalk = require('chalk');
 var readlineSync = require('readline-sync');
 
 var userName = readlineSync.question("Please Enter Your Name: ");
-console.log(chalk.blue("Welcome! ") + chalk.bold(userName) + chalk.blue(" to Guess the Song! Challenge"));
+console.log(chalk.blue("Welcome! ") + chalk.underline.bold.yellowBright(userName) + chalk.blue(" to Guess the Song! Challenge") + "\n");
 
-var score = 0;
-var highScore = 0;
+var userScore = 0;
+
 function lyricsQuiz(lyric, song, artist){
-  console.log(lyric);
+  console.log(chalk.italic.bgBlack(lyric));
   var userAnswer = readlineSync.question("Enter Name of the Song: ");
+  console.log("");
 
-  if (userAnswer.toLowerCase() === song.toLowerCase()){
-    console.log("Yay! You guessed it right.");
-    console.log("The song is " + song.bold() + " by " + artist.bold());
-    score = score + 1;
+  if (userAnswer.toLowerCase() == song.toLowerCase())
+  {
+    console.log(chalk.greenBright("Yay! You guessed it right."));
+    console.log(chalk.green("The song is " + song + " by " + artist), "\n");
+    userScore = userScore + 1;
   }
-  else{
-    console.log("Sorry. You guessed wrong.");
+  else
+  {
+    console.log(chalk.red("Sorry. You guessed wrong."));
+    console.log(chalk.red("The song is " + song + " by " + artist), "\n");
   }
-  console.log("Your Current Score: ", score);
   }
 
 var lyrics = [{
@@ -26,13 +29,26 @@ var lyrics = [{
 }, {
   lyric: "\"Seasons change and our love went cold. Feed the flame 'cause we can't let it go. Run away, but we're running in circles. Run away, run away.\"", song: "Circles", artist: "Post Malone"
 }, {
-  lyric: "\"It's just you and I, and no other guys. We got no interruptions and we both feelin' the vibe. Say I'm not your type. But I know what's on your mind. We can talk about nothin' or we can see what it's like\"", song: "beside you", artist: "keshi" 
+  lyric: "\"It's just you and I, and no other guys. We got no interruptions and we both feelin' the vibe. Say I'm not your type. But I know what's on your mind. We can talk about nothin' or we can see what it's like\"", song: "beside u", artist: "keshi" 
 }, {
   lyric: "\"Constellations of stars. Murals on city walls. I don't see nobody but you. You're my vice, you're my muse. You're a nineteenth floor view. I don't see nobody but you\"", song: "double take", artist: "dhruv"
 }];
 
 for (i = 0; i < lyrics.length; i++){
   j = i + 1;
-  console.log("Lyric No: ", j);
+  console.log(chalk.magenta("Lyric No: ", j));
   lyricsQuiz(lyrics[i].lyric, lyrics[i].song, lyrics[i].artist);
 }
+
+var scores = [{
+  name: "Shashank", score: 3
+},{
+  name: "Shank", score: 2
+}];
+scores.push({name: userName, score: userScore});
+
+console.log(chalk.magenta("Scores: "));
+for ( var i = 0; i <= scores.length; i++){
+  console.log(chalk.cyan(scores[i].name + ": " + scores[i].score));
+}
+
